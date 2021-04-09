defce4a5483a6bfa3cfa9ced1f17f7b508d2cca3
sunyuhua <sun.yuhua@geexek.com>
2021-04-09 11:53:31 +0800

    初始化项目

---
+++ src/main/java/com/chen/www/spingbootstart/SpingbootstartApplication.java
@@ -0,0 +1,13 @@
+package com.chen.www.spingbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingbootstartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chen/www/spingbootstart/SpingbootstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.www.spingbootstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
